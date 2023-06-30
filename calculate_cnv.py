849f95ec5d68bfa4d303d8535bb3e58ba67ff79a
chsmiss <55265593+chsmiss@users.noreply.github.com>
2023-06-30 14:19:55 +0800

    Update calculate_cnv.py

--- calculate_cnv.py
+++ calculate_cnv.py
@@ -23,7 +23,7 @@ class find_amp:
 			self.chrom_dir[ref] = []
 			self.chrom_dir[ref].append(Interval(0, 0))
 
-#计算位点左侧指定长度区域测序深度，默认长度是interval
+#Calculates the sequencing depth of the specified length region to the left of the locus, the default length is interval
 	def interval_cov(self,samfile_name,chr_n,site,interval_size=None):
 		reads=0
 		interval_size=self.interval
@@ -35,7 +35,7 @@ class find_amp:
 			reads=reads+sum(k)
 		return(reads/interval_size)
 
-	#同上，计算右侧区域测序深度
+	#As above, calculate the sequencing depth of the right-hand region
 	def interval_cov1(self,samfile_name,chr_n,site,interval_size=None):
 		reads=0
 		interval_size = self.interval
@@ -49,7 +49,7 @@ class find_amp:
 			reads=reads+sum(k)
 		return(reads/interval_size)
 
-	#找连续有测序深度的区域
+	#Finding contiguous regions with sequencing depth
 	def find_amplicon(self,sam,start_c,start_s,cov_t=5):
 		interval=self.interval
 		cov_t = self.cov
@@ -57,31 +57,31 @@ class find_amp:
 		if l_site >= sam.get_reference_length(start_c):
 			l_site = sam.get_reference_length(start_c) - 1
 
-			#左边连续区域
+			#Continuous area on the left
 		while self.interval_cov(sam,start_c,l_site)>cov_t or self.interval_cov(sam,start_c,l_site,interval_size=10*interval)>cov_t:
 			l_site=l_site-interval
 			if l_site <= 0:
 				break
 		r_site = start_s
-		#右边连续区域
+		#Continuous area on the right
 		while self.interval_cov1(sam,start_c,r_site)>cov_t or self.interval_cov1(sam,start_c,r_site,interval_size=10*interval)>cov_t:
 			r_site = r_site+interval
 			if r_site >= sam.get_reference_length(start_c):
 				break
-	#返回左端点和右端点
+	#Return to left endpoint and right endpoint
 		return(l_site,r_site)
 
-	#判断断点在不在已检测扩增中
+	#Determine if the breakpoint is in the detected amplification
 	def isinchrom_dir(self,chr,pos):
 		chrom_dir = self.chrom_dir
 		for line in chrom_dir[chr]:
 			if pos in line:
-	#在已知扩增区域的话，返回布尔值True和扩增区域信息
+	#If the amplification region is known, the Boolean value True and the amplification region information are returned.
 				return (True,line)
 
 		return (False,line)
 
-	#主要功能，遍历断点文件进行处理
+	#Main functions, traversing breakpoint files for processing
 	def process_coverage(self,bam=None,support_reads=5):
 		bk_file = self.discbk
 		chrom_dir = self.chrom_dir
@@ -90,57 +90,57 @@ class find_amp:
 		s_t = time.time()
 		f_bkline = open(bk_file,'r')
 		bklines = f_bkline.readlines()
-		#创建一个结果文件
+		#Create a results file
 		outfile_n = bk_file+'.amplicon'
 		f_out = open(outfile_n,'w')
-		#count3计数读取了多少行
+		#count3 counts how many rows have been read
 		count3 = 0
 		for line in bklines:
 			count3 = count3 + 1
 			if count3%1000 == 0:
 				print(count3)
 				print(time.time()-s_t)
-			#去除比对到不确定染色体的断点对
+			#Removal of breakpoint pairs that match to uncertain chromosomes
 			if 'chrUn' in line or 'random' in line:
 				continue
 
 			bkline_list = line.split('\t')
 
-			#小于5个支持reads的断点对不要
+			#Less than 5 breakpoint pairs with reads support do not
 			if int(bkline_list[4]) <= support_reads:
 				continue
 
-			#读取断点对的两端位于的染色体
+			#Read the chromosomes located at both ends of the breakpoint pair
 			s_chr = bkline_list[0]
 			e_chr = bkline_list[2]
 
-			#读取断点对两端区间，暂时用区间中点表示端点位置
+			#Read the interval between the ends of the breakpoint pair, temporarily using the midpoint of the interval to indicate the endpoint position
 			start_site = int((int(bkline_list[1].strip('[').strip(']').split('.')[-1])+int(bkline_list[1].strip('[').strip(']').split('.')[0])+1)/2)
 			end_site = int((int(bkline_list[3].strip('[').strip(']').split('.')[-1])+int(bkline_list[3].strip('[').strip(']').split('.')[0])+1)/2)
 
-			#端点坐标小于0的不要
+			#Do not have endpoints with coordinates less than 0
 			if start_site <= 0 or end_site <= 0:
 				continue
 
-			#调用isinchrom_dir函数，判断左端是否在已收集扩增区域
+			#Call the isinchrom_dir function to determine if the left end is in the collected amplification region
 			if self.isinchrom_dir(s_chr,start_site)[0]:
-				#如果在已知扩增区域，把区域位置拿出来
+				#If in a known amplification region, take out the region location
 				first_l,first_r = self.isinchrom_dir(s_chr,start_site)[1].lower_bound,self.isinchrom_dir(s_chr,start_site)[1].upper_bound
 
 			else:
-				#如果不在，调用find_amplicon以端点为起始搜索扩增区域，并把这个区域加入扩增区域字典
+				#If not, call find_amplicon to search for the augmented region starting with the endpoint and add this region to the augmented region dictionary
 
 				first_l,first_r = self.find_amplicon(samfile,s_chr,start_site)
 				chrom_dir[s_chr].append(Interval(first_r,first_l))
 
-			#同上，判断右端点
+			#As above, determine the right endpoint
 			if self.isinchrom_dir(e_chr,end_site)[0]:
 				second_l,second_r = self.isinchrom_dir(e_chr,end_site)[1].lower_bound,self.isinchrom_dir(e_chr,end_site)[1].upper_bound
 			else:
 				second_l,second_r = self.find_amplicon(samfile,e_chr,end_site)
 				chrom_dir[e_chr].append(Interval(second_l,second_r))
 
-			#左右端点都位于大于某个大小的区间才保留
+			#Left and right endpoints are only retained if they lie in an interval greater than a certain size
 			if (first_r - first_l) > 2000 and (second_r - second_l)>2000:
 				out_line = 'first_bk_interval'+'\t'+str(first_l)+'\t'+str(first_r)+'\t'+'first_len'+'\t'+str(first_r - first_l)+'\t''second_bk_interval'+'\t'+str(second_l)+'\t'+str(second_r)+'\t'+'second_len'+'\t'+str(second_r - second_l)+'\t'+line
 				f_out.write(out_line)
